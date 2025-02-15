bd7dc84a551f35dcd06b1a271e806a67eec71345
Antoniel <antonielzeferino0@gmail.com>
2025-02-14 22:14:45 -0300

    import correction

--- src/app/(pages)/poema/criar/page.tsx
+++ src/app/(pages)/poema/criar/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import CreatePoemForm from './CreatePoemForm';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
 
 const CreatePoemPage = async () => {
