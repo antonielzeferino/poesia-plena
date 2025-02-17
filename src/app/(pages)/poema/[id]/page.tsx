7c87de86496863e51fc316933b436f1fa2dd63d0
antoniel <antonielzeferino0@gmail.com>
2025-02-17 15:37:36 -0300

    change text style

--- src/app/(pages)/poema/[id]/page.tsx
+++ src/app/(pages)/poema/[id]/page.tsx
@@ -1,3 +1,4 @@
+import MenuSidebar from "@/components/MenuSideBar";
 import { prisma } from "@/lib/prisma";
 import withAuth from "@/lib/withAuth";
 import { notFound } from "next/navigation";
@@ -30,10 +31,11 @@ async function PoemPage({ params }: { params: Promise<{ id: string }> }) {
 
   return (
     <div className="flex flex-col items-center justify-center">
+      <MenuSidebar />
       <main className="w-full md:px-6 pb-4">
         <h1 className="text-2xl text-foreground mb-4 font-light font-mono text-center">{poem.title}</h1>
         <p className="text-sm text-muted">Por {poem.author.username}</p>
-        <p className="text-foreground text-justify break-words hyphens-auto whitespace-pre-wrap max-w-max">{poem.content}</p>
+        <p className="text-foreground whitespace-pre-wrap max-w-max">{poem.content}</p>
       </main>
     </div>
   );
