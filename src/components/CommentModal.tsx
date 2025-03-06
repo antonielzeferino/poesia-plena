6fb490deb007842b5339f42b1097cdfbd9c9243c
antoniel <antonielzeferino0@gmail.com>
2025-03-06 14:27:48 -0300

    conflic corrrection

--- src/components/CommentModal.tsx
+++ src/components/CommentModal.tsx
@@ -87,8 +87,7 @@ const CommentModal = ({
         </button>
         <h2 className="text-lg font-semibold mb-4">Comentários</h2>
 
-        {/* Comentários roláveis */}
-        <div className="flex-1 py-4 px-2 space-y-3 overflow-y-auto max-h-[calc(100vh-200px-80px)] ">
+        <div className="flex-1 py-4 px-2 space-y-3 overflow-y-auto max-h-[calc(100vh-200px-80px)]">
           {comments.length > 0 ? (
             comments.map((comment) => (
               <div
