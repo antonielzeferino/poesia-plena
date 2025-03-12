ea565709243dd8ab3844d30eb86434f9cbd50ca7
Antoniel <antonielzeferino0@gmail.com>
2025-03-12 14:33:32 -0300

    auth style change

--- src/app/(pages)/auth/signin/page.tsx
+++ src/app/(pages)/auth/signin/page.tsx
@@ -33,30 +33,30 @@ const Signin = () => {
     <div className="flex flex-col items-center justify-center flex-grow px-4">
       <h2 className="font-bold text-3xl mb-6 text-foreground text-center">Bem Vindo(a)!</h2>
 
-      <div className="p-6 shadow-lg w-full max-w-sm min-h-[420px] bg-white rounded-tl-[5rem] rounded-br-[5rem] flex flex-col items-center">
-        <h2 className="text-xl font-semibold text-center text-gray-800 sm:mt-4">Entrar</h2>
+      <div className="p-6 shadow-lg w-full max-w-sm min-h-[420px] bg-contrast rounded-tl-[5rem] rounded-br-[5rem] flex flex-col items-center">
+        <h2 className="text-xl font-semibold text-center text-foreground sm:mt-4">Entrar</h2>
 
         <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 px-4 flex-grow overflow-auto">
           <div className="flex flex-col">
-            <label htmlFor="username" className="text-sm font-medium text-gray-700">Usuário</label>
+            <label htmlFor="username" className="text-sm font-medium text-muted">Usuário</label>
             <input
               type="text"
               id="username"
               name="username"
               placeholder="Digite seu usuário"
-              className="p-3 border rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-400 text-background"
+              className="p-3 border border-muted rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-400 text-foreground"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="flex flex-col">
-            <label htmlFor="password" className="text-sm font-medium text-gray-700">Senha</label>
+            <label htmlFor="password" className="text-sm font-medium text-muted">Senha</label>
             <input
               type="password"
               id="password"
               name="password"
               placeholder="Digite sua senha"
-              className="p-3 border rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-400 text-background"
+              className="p-3 border border-muted rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-400 text-foreground"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
@@ -70,7 +70,7 @@ const Signin = () => {
           
           {error && <p className="text-red-500 text-center mt-2">{error}</p>}
         </form>
-        <p className="text-sm text-gray-600 text-center pb-2">
+        <p className="text-sm text-foreground text-center pb-2">
           Ainda não tem uma conta?{" "}
           <Link href="/auth/signup" className="text-blue-600 font-medium hover:underline">
             Cadastre-se
