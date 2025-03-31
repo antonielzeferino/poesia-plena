09520701ce3127c917a8332bc3cb9dee670ebf20
antoniel <antonielzeferino0@gmail.com>
2025-03-30 22:03:22 -0300

    add update profile route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
@@ -19,4 +19,25 @@ export async function GET() {
   }
 
   return NextResponse.json(user);
+}
+
+export async function PUT(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Não autenticado" }, { status: 401 });
+  }
+
+  const { name, bio } = await req.json();
+
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { username: session.user.username },
+      data: { name, bio },
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    return NextResponse.json({ error: "Erro ao atualizar usuário" }, { status: 500 });
+  }
 }
